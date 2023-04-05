991d4d61ec619a0ce39efa8cb88662c3fc8275a1
Jackychung0522 <stp77770z@gmail.com>
2023-04-05 23:06:44 +0800

    resize

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# hw1-Html-CSS-and-JavaScript
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-8d59dc4de5201274e310e4c54b9627a8934c3b88527886e3b421487c677d23eb.svg)](https://classroom.github.com/a/vtMjwcap)
+# hw2-handling-input-events
+This is the starter code of [2023-Programming User Interface Homework](https://hackmd.io/@akairisu/HkUibgmx3)
 - 姓名: 鍾昀諺
-- 實作: 在URL上輸入index.html的絕對路徑
+- 網站連結: https://superb-marigold-51f857.netlify.app
+- 我的設計: 目前在網頁上實作的功能都是可行的，而在手機上的touch系列，做到了另一隻手指觸及停止功能，以及水平大小變化，另一隻手指移開維持大小，恢復觸及仍能改變大小，與第三隻手指觸碰回到原本大小，還有限制方塊不能小於某個寬度的功能。
 - 加分項目: 無
 - 有趣之處: 無
--- hw2/input.js
+++ hw2/input.js
@@ -143,7 +143,7 @@ const State = {
               else{
                 this.setColor("blue");
                 this.isDragging = false;
-                this.setState(State.IDLE);
+                this.setState(State.SELECTED);
                 console.log(this.state);
               }
             
@@ -295,11 +295,11 @@ const State = {
         }
         if (event.touches.length >=3 && (this.state == State.RESIZE ) ){ 
             this.setState(State.IDLE);
-            this.element.style.transform=`scaleX(${this.lastScalex})`;
+            this.element.style.transform=`scaleX(1)`;
             console.log(event.touches.length);
             //this.lastScalex=this.scalex;
             console.log("lsatS:"+this.lastScalex);
-            return;
+            
             //console.log("two");
                         //this.setColor('red');
           }
